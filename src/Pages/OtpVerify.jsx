d7a81eb486fbfd2a611d5b2e53f007d437cb4605
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2023-09-22 20:41:00 +0500

    first commit

--- src/Pages/OtpVerify.jsx
+++ src/Pages/OtpVerify.jsx
@@ -3,16 +3,16 @@ import React from "react";
 const OtpVerify = () => {
   return (
     <>
-      <div className="otp-verify-container">
+      <div className="otp-verify-page">
         <h1>OTP CODE</h1>
         <h2>CODE</h2>
-        <div className="otp-input-box">
-          <input type="number" />
-          <input type="number" />
-          <input type="number" />
-          <input type="number" />
-          <input type="number" />
-          <input type="number" />
+        <div className="otp-input-container">
+          <input type="text" className="otp-input-box" maxLength={1}/>
+          <input type="text" className="otp-input-box" maxLength={1}/>
+          <input type="text" className="otp-input-box" maxLength={1}/>
+          <input type="text" className="otp-input-box" maxLength={1}/>
+          <input type="text" className="otp-input-box" maxLength={1}/>
+          <input type="text" className="otp-input-box" maxLength={1}/>
         </div>
       </div>
     </>
