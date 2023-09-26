702fe04c61a16ca1e0df0b9a85209f2bcef1afe6
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2023-09-26 20:42:33 +0500

    fetched all users

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import userPic from '../assets/user-image.jpg';
 
 const UserCard = (props) => {
     const {user}=props
+    const userImage=user.image;
   return (
     <>
     <div className="user_card">
+      <img src={userImage ? `http://localhost:5000/${userImage}` : userPic} 
+      alt="user image" 
+      width={"100px"} 
+      height={"100px"} 
+      id='user-image'/>
+        <div>
         <h1>{user.name}</h1>
         <h2>{user.email}</h2>
         <h2>{user.phone}</h2>
+        </div>
     </div>
     </>
   )
